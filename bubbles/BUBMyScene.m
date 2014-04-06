f8d9c094ec21c823aebce073fe003b062e5e5a2b
Gavin Anderegg <gavin@anderegg.ca>
2014-04-06 12:12:32 -0300

    Physics bodies + randomized bezier

--- bubbles/BUBMyScene.m
+++ bubbles/BUBMyScene.m
@@ -13,6 +13,7 @@
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
     }
     return self;
 }
@@ -26,12 +27,27 @@
         SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:bubbleTexture size:CGSizeMake(32.0, 32.0)];
         sprite.blendMode = SKBlendModeAdd;
         sprite.alpha = 0.9;
+        sprite.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:sprite.size.width/2];
+        sprite.physicsBody.dynamic = YES;
+        sprite.physicsBody.affectedByGravity = NO;
         
         sprite.position = touchLocation;
         
         UIBezierPath *sine = [[UIBezierPath alloc] init];
         [sine moveToPoint:CGPointMake(0.0, 0.0)];
-        [sine addCurveToPoint:CGPointMake(0.0, 200.0) controlPoint1:CGPointMake(-100.0, 100.0) controlPoint2:CGPointMake(100.0, 100.0)];
+        
+        float bezLeft = (rand() % (-50 - -150)) + -150;
+        float bezRight = (rand() % (150 - 50)) + 50;
+        float bezTop = (rand() % (200 - 100)) + 100;
+        float bezMid = bezTop / 2;
+        
+        if ((rand() % 2) > 0) {
+            float temp = bezLeft;
+            bezLeft = bezRight;
+            bezRight = temp;
+        }
+        
+        [sine addCurveToPoint:CGPointMake(0.0, bezTop) controlPoint1:CGPointMake(bezLeft, bezMid) controlPoint2:CGPointMake(bezRight, bezMid)];
         CGPathRef sinePath = sine.CGPath;
         
         SKAction *followTrack = [SKAction followPath:sinePath asOffset:YES orientToPath:NO duration:1.0];
